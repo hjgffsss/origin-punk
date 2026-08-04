bffb34d6c92c739a3d378b76515b53050bf9db62
hjgffsss <116235961+hjgffsss@users.noreply.github.com>
2026-08-04 14:21:07 +0600

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OriginPunkApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <OriginPunkApp />
+  </React.StrictMode>
+);
